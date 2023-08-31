166b4472afd27383aa2e6a56a900b721949c59dc
laismullerrr <lais.muller@hotmail.com>
2023-08-30 22:13:17 -0300

    feat: :sparkles: função para ordenar e exibir a lista de propriedades ordenada

--- app.js
+++ app.js
@@ -12,3 +12,22 @@ function inserirPropriedades() {
     inserirPropriedades();
   }
 }
+
+const listaPropriedades = [];
+function exibirListaPropriedades() {
+  
+  const propriedadesOrdenadas = listaPropriedades.sort();
+  const listaPropriedadesVazia = listaPropriedades.length;
+
+  if (listaPropriedadesVazia === 0) {
+    console.log('Ops, parece que você não inseriu nada por aqui.');
+  } else {
+      propriedadesOrdenadas; 
+      console.log('\nAgradeçemos por utilizar nosso sistema. Aqui estão suas propriedades ordenadas:');
+      listaPropriedades.forEach((propriedade) => {
+        console.log(propriedade);
+      });
+    }
+  }
+
+inserirPropriedades()
