5500009c6089a1476043598d473b4f12380d09cf
laismullerrr <lais.muller@hotmail.com>
2023-08-31 17:45:23 -0300

    feat: :sparkles: add else if em inserirPropriedades() para não permitir entrada de números

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-import rl from 'readline-sync';
+import rl from "readline-sync";
+
 
 function inserirPropriedades() {
-  let entrada = (rl.question('Por favor, digite uma propriedade CSS ou "SAIR" para encerrar: ')).toLowerCase();
-  if (entrada === 'sair') {
+  let entrada = rl
+    .question('Por favor, digite uma propriedade CSS ou "SAIR" para encerrar: ')
+    .toLowerCase();
+  if (entrada === "sair") {
     exibirListaPropriedades();
-  } else if (entrada.trim() === '') {
-    console.log('Ops, parece que essa entrada é inválida. Por favor, insira uma propriedade CSS ou digite "SAIR" para encerrar.');
+  } else if (entrada.trim() === "" ) {
+    console.log(
+      'Ops, parece que essa entrada é inválida. Por favor, insira uma propriedade CSS ou digite "SAIR" para encerrar.'
+    );
     inserirPropriedades();
+    } else if (!isNaN(entrada)) {
+      console.log('Ops, a entrada não pode conter números. Por favor, insira uma propriedade CSS válida ou digite "SAIR" para encerrar.');
+      inserirPropriedades();
   } else {
     listaPropriedades.push(entrada);
     inserirPropriedades();
@@ -15,19 +23,21 @@ function inserirPropriedades() {
 
 const listaPropriedades = [];
 function exibirListaPropriedades() {
-  
-  const propriedadesOrdenadas = listaPropriedades.sort()
-  const listaPropriedadesVazia = listaPropriedades.length
+  const propriedadesOrdenadas = listaPropriedades.sort();
+  const listaPropriedadesVazia = listaPropriedades.length;
 
   if (listaPropriedadesVazia === 0) {
-    console.log('Ops, parece que você não inseriu nada por aqui. Agradeçemos por utilizar nosso sistema!')
+    console.log(
+      "Ops, parece que você não inseriu nada por aqui. Agradeçemos por utilizar nosso sistema!"
+    );
   } else {
-      propriedadesOrdenadas
-      console.log('\nAqui estão suas propriedades ordenadas:')
-      listaPropriedades.forEach((propriedade) => {
-        console.log("- "+propriedade)})
-      console.log('\nAgradeçemos por utilizar nosso sistema!')
-    }
-  };
+    propriedadesOrdenadas;
+    console.log("\nAqui estão suas propriedades ordenadas:");
+    listaPropriedades.forEach((propriedade) => {
+      console.log("- " + propriedade);
+    });
+    console.log("\nAgradeçemos por utilizar nosso sistema!");
+  }
+}
 
-inserirPropriedades()
+inserirPropriedades();
