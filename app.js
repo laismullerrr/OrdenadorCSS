59b161a6b1c5ee57e6d310e49492ad36f2d7fa82
laismullerrr <lais.muller@hotmail.com>
2023-08-30 22:01:56 -0300

    feat: :sparkles: função para inserção das propriedades

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import rl from 'readline-sync';
 
-function input(string) {
-  return rl.question(string);
+function inserirPropriedades() {
+  let entrada = (rl.question('Por favor, digite uma propriedade CSS ou "SAIR" para encerrar: ')).toLowerCase();
+  if (entrada === 'sair') {
+    exibirListaPropriedades();
+  } else if (entrada.trim() === '') {
+    console.log('Ops, parece que essa entrada é inválida. Por favor, insira uma propriedade CSS ou digite "SAIR" para encerrar.');
+    inserirPropriedades();
+  } else {
+    listaPropriedades.push(entrada);
+    inserirPropriedades();
+  }
 }
